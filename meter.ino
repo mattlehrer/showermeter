9565d14a0b3e0467c79ba033fa086a7a2a538e78
Matt Lehrer <lehrerm@gmail.com>
2021-12-12 03:22:44 +0100

    WIP: add total shower stats

--- meter.ino
+++ meter.ino
@@ -12,7 +12,8 @@ const int PULSES_PER_LITER = 450; // https://www.seeedstudio.com/blog/2020/05/11
 
 volatile int  Pulse_Count;
 unsigned int  Liter_per_hour;
-unsigned long Current_Time, Loop_Time;
+unsigned int  Shower_Seconds, Shower_Minutes;
+unsigned long Current_Time, Loop_Time, Start_Time, Most_Recent_Pulse_Time;
 
 void setup()
 { 
@@ -24,7 +25,9 @@ void setup()
    attachInterrupt(digitalPinToInterrupt(2), Detect_Rising_Edge, RISING);
                                      
    Current_Time = millis();
+   Start_Time = millis();
    Loop_Time = Current_Time;
+	 Most_Recent_Pulse_Time = 0;
 } 
 
 void loop ()    
@@ -38,8 +41,34 @@ void loop ()
       Serial.print(Liter_per_hour, DEC);
       Serial.println(" Liter/hour");
    }
+
+	 // Check for end of shower
+	 if(Most_Recent_Pulse_Time > 0 && Current_Time >= (Most_Recent_Pulse_Time + 10000))
+	 {
+		 Serial.println("10 seconds elapsed since last pulse");
+		 Serial.println("Full shower statistics:");
+		 Serial.print("Total pulses: ");
+		 Serial.println(Pulse_Count);
+		 Serial.print("Total liters: ");
+		 Serial.println(Pulse_Count * PULSES_PER_LITER);
+
+		Shower_Seconds = (Current_Time - Start_Time) / 1000;
+		Shower_Minutes = Shower_Seconds / 60;
+
+		 Serial.print("Total time: ");
+		 Serial.print(Shower_Minutes);
+		 Serial.print(":");
+		 // TODO: format seconds to be 2 digits
+		 Serial.println(Shower_Seconds % 60);
+
+		 // Reset all variables for next shower
+		 Start_Time = millis();
+		 Pulse_Count = 0;
+		 Most_Recent_Pulse_Time = 0;
+	 }
 }
 void Detect_Rising_Edge ()
 { 
    Pulse_Count++;
+	 Most_Recent_Pulse_Time = millis();
 } 
